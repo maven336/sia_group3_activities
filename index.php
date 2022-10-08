536112774b8f90700faf8481fd7aff9e720c06de
maven336 <mavenquijano36@gmail.com>
2022-10-08 10:09:32 +0800

    added username for sprint1

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <h1>Welcome to Electric Billing System</h1>
+     <a href="login.php">LOGIN</a>
+ </body>
+</html>
